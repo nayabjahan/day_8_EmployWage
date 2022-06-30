ca8f98444f52dcb39d44c6e7c51d22b5d6189256
nayab jahan <nayabzafir@gmail.com>
2022-06-30 14:49:36 +0530

    Added: Employ Attence

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,7 @@
 public class Welcome {
     public static void main(String[] args){
+        // printing welcome message
+
         System.out.println("Welcome to employ wage computation problem");
     }
 }
